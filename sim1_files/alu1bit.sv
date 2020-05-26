600db9dc0ddf44e24006f9025e0016bd4c09e468
Gur Telem <gur111telem@gmail.com>
2020-05-26 15:10:45 +0300

    Implement ALU and ALU64

--- sim1_files/alu1bit.sv
+++ sim1_files/alu1bit.sv
@@ -10,7 +10,31 @@ module alu1bit (
 
 // Put your code here
 // ------------------
+parameter B=10,C=6,D=6,E=3,F=1,G=8;
+logic fas_sum, g1_out;
+logic g2_out, g3_out, g4_out, mux_out;
 
+fas #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) FAS (
+        .cout(cout),
+        .cin(cin),
+        .a_ns(g1_out),
+        .s(fas_sum),
+        .a(a),
+        .b(b)
+);
+
+NOT #(.Tpdlh(B), .Tpdhl(C)) g1 (.Z(g1_out), .A(op[0]));
+OR2 #(.Tpdlh(D), .Tpdhl(E)) g2 (.Z(g2_out), .A(a), .B(b));
+XOR2 #(.Tpdlh(F), .Tpdhl(G)) g3 (.Z(g3_out), .A(a), .B(b));
+NOT #(.Tpdlh(B), .Tpdhl(C)) g4 (.Z(g4_out), .A(g2_out));
+mux4 #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) MUX (
+    .Z(s),
+    .sel(op),
+    .d0(g4_out),
+    .d1(g3_out),
+    .d2(fas_sum),
+    .d3(fas_sum)
+);
 
 // End of your code
 
--- sim1_files/alu64bit.sv
+++ sim1_files/alu64bit.sv
@@ -10,7 +10,28 @@ module alu64bit (
 
 // Put your code here
 // ------------------
+parameter B=10,C=6,D=6,E=3,F=1,G=8;
 
+logic alu_cin[64:0];
+// cin = alu_cin[0];
+assign alu_cin[0] = alu_cin[64];
+// cout = alu_cin[64];
+
+genvar i;
+generate
+    for (i = 0; i<64; i++)
+    begin
+        alu1bin #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) ALU (
+            .a(a[i]),
+            .b(b[i]), 
+            .cin(alu_cin[i]), 
+            .cout(alu_cin[i+1]), 
+            .p(op), 
+            .s(s[i])
+        );
+    end
+
+endgenerate
 
 // End of your code
 
