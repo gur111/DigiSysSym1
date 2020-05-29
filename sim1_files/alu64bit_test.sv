cfb767e0bdfa3610d2ef350f4bd6761f2ada4a09
Gur Telem <gur111telem@gmail.com>
2020-05-29 18:35:03 +0300

    Add alu64 test

--- sim1_files/alu64bit_test.sv
+++ sim1_files/alu64bit_test.sv
@@ -1,10 +1,35 @@
 // 64-bit ALU test bench template
 module alu64bit_test;
+    logic [63:0] a;
+    logic [63:0] b;
+    logic cin;
+    logic [1:0] op;
+    logic [63:0] s;
+    logic cout;
 
-// Put your code here
-// ------------------
+    localparam B=10, C=6, D=6, E=3, F=1, G=8;
+
+    initial begin
+        op = 2'b11;
+        cin = 1'b0;
+        a = {64{1'b0}};
+        b = {64{1'b0}};
+
+        #2000;
+        cin = 1'b1;
+
+        #2000;
 
 
-// End of your code
+    end
+
+    alu64bit #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) alu (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .op(op),
+        .s(s),
+        .cout(cout)
+    );
 
 endmodule
