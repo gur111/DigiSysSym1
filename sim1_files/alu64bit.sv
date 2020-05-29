a7d945e79b6afea46edf3592e02300fde6af00e0
Gur Telem <gur111telem@gmail.com>
2020-05-29 18:35:37 +0300

    Finish alu64

--- sim1_files/alu64bit.sv
+++ sim1_files/alu64bit.sv
@@ -13,20 +13,20 @@ module alu64bit (
 parameter B=10,C=6,D=6,E=3,F=1,G=8;
 
 logic alu_cin[64:0];
-// cin = alu_cin[0];
-assign alu_cin[0] = alu_cin[64];
-// cout = alu_cin[64];
+
+assign alu_cin[0] = cin;
+assign cout = alu_cin[64];
 
 genvar i;
 generate
     for (i = 0; i<64; i++)
     begin
-        alu1bin #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) ALU (
+        alu1bit #(.B(B), .C(C), .D(D), .E(E), .F(F), .G(G)) ALU (
             .a(a[i]),
             .b(b[i]), 
             .cin(alu_cin[i]), 
             .cout(alu_cin[i+1]), 
-            .p(op), 
+            .op(op), 
             .s(s[i])
         );
     end
